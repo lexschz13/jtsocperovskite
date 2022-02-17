86b7a41bef223053c25a122f800488af06d6b025
lexschz13 <77620104+lexschz13@users.noreply.github.com>
2022-02-17 12:59:50 +0100

    Update dos.py

--- dos.py
+++ dos.py
@@ -3,8 +3,8 @@ from .qarray import *
 
 
 def dos(x, H, idxs, dump):
-    w,U = np.linalg.eig(H)
-    G = np.zeros(x.size,dtype=np.complex128)
+    w,U = H.eig()
+    G = qzeros(x.size,dtype=np.complex128)
     for i in idxs:
         #[eigen,frequency], sum over eigenstates
         G += np.sum(abs(U[i,:,None])**2/((x[None,:]-w[:,None]) + 1j*dump), axis=0)
