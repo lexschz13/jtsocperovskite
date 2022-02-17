6118895a8a90a043afe90411408f85ac7111c9c1
lexschz13 <77620104+lexschz13@users.noreply.github.com>
2022-02-17 12:53:41 +0100

    Update dos.py

--- dos.py
+++ dos.py
@@ -6,5 +6,6 @@ def dos(x, H, idxs, dump):
     w,U = np.linalg.eig(H)
     G = np.zeros(x.size,dtype=np.complex128)
     for i in idxs:
+        #[eigen,frequency], sum over eigenstates
         G += np.sum(abs(U[i,:,None])**2/((x[None,:]-w[:,None]) + 1j*dump), axis=0)
     return -G.imag/np.pi
